5e0aa4840bdee47c55b24d3e4178e740389648be
unknown <alanpaullo@yahoo.com.br>
2025-07-22 22:49:07 -0300

    ajuste de yml

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,17 @@
 const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    reportDir: 'cypress/reports/html',
+    overwrite: false,
+    html: true,
+    json: true,
+    charts: true,
+    embeddedScreenshots: true,
+    inlineAssets: true
+  },
+  video: false,
   e2e: {
     setupNodeEvents(on, config) {
       require('cypress-mochawesome-reporter/plugin')(on);
@@ -16,16 +27,5 @@ module.exports = defineConfig({
         openMode: 5
       }
     }
-  },
-  reporter: 'cypress-mochawesome-reporter',
-  reporterOptions: {
-    reportDir: 'cypress/reports/html',
-    overwrite: false,
-    html: true,
-    json: true,
-    charts: true,
-    embeddedScreenshots: true,
-    inlineAssets: true
-  },
-  video: false
+  }
 });
